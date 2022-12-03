ec436fd622adbd6fc4d1b6e632efa6002a330698
bharatitmg <111448458+bharatitmg@users.noreply.github.com>
2022-12-03 11:11:56 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-let x = "";
-let buttons=document.querySelectorAll('.button');
+let string = "";
+let buttons = document.querySelectorAll('.button');
 Array.from(buttons).forEach((button)=>{
-    button.addEventListener('click',(e)=>{
-        if(e.targrt.innerHTML == "="){
-            x=eval(x);
-            document.querySelector('input').value = x;
+    button.addEventListener('click', (e)=>{
+        if (e.target.innerHTML == "="){
+            string = eval(string);
+            document.querySelector('input').value = string;
         }
-        else if(e.target.innerHTML == 'C'){
-            x="";
-            document.querySelector('input').value = x;
-        }
+        else if  (e.target.innerHTML == "C"){
+            string = "";
+            document.querySelector('input').value = string;
+        } 
         else{
             console.log(e.target)
-            x=x+e.target.innerHTML;
-            document.querySelector('input').value=x;
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
         }
     })
 })
